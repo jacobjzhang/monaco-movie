7df1d962b5f4650936d89565ebc7adaad6ace40f
Jacob J. Zhang <jacobjzhang@gmail.com>
2018-07-14 12:37:35 -0400

    Beginning to implement the build pipeline properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,41 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+Error.stackTraceLimit = Infinity;
 
 module.exports = {
-	mode: 'production',
 	entry: {
-		"app": './index.js',
-		"editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
-		// "json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
-		// "css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
-		// "html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
-		// "ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
+		"app": './src/index.js',
+		"editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js'
 	},
 	output: {
 		globalObject: 'self',
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
-	module: {
-		rules: [{
-			test: /\.css$/,
-			use: [ 'style-loader', 'css-loader' ]
-		}]
-	},
-	plugins: [
-		new webpack.IgnorePlugin(/^((fs)|(path)|(os)|(crypto)|(source-map-support))$/, /vs(\/|\\)language(\/|\\)typescript(\/|\\)lib/),
-		new UglifyJSPlugin()
-	],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  optimization: {
+    minimize: true,
+    namedModules: true,
+    concatenateModules: true,
+  },
+  plugins: [
+    new MonacoWebpackPlugin(
+      {
+        languages: ['javascript'],
+        features: ['links']
+      }
+    ),
+    new HtmlWebpackPlugin({
+      title: 'Monaco Movie',
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
 };
